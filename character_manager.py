a5a97c8098fc206ed9375a5556382c58da7d11bd
りんりん <kurokamisuketto@gmail.com>
2026-05-14 16:57:34 +0900

    hy

--- character_manager.py
+++ character_manager.py
@@ -22,4 +22,10 @@ class CharacterManager:
         self.relation = relation
         self.memo = memo
 
-
+def create(self, char_data):
+        # ここに、実際にDBのテーブルに保存するSQLコードを書くんだよ
+        # core.py から「封筒（char_data）」が届く想定だね！
+        print(f"データベースに {char_data['name']} を保存する処理を開始します...")
+        
+        # 本来はここに conn = sqlite3.connect... みたいな保存処理が入る
+        return True
